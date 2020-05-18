0df324ae8f703282f70f6bdc50a7e43c0de26c07
Colette <colette.rs.shaw@gmail.com>
2020-05-18 17:04:37 +0100

    Change package name

---
+++ src/main/java/com/test/crss/CheckBoard.java
@@ -0,0 +1,11 @@
+package com.test.crss;
+
+public class CheckBoard {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean checkBoard(int[][] board) {
+        return true;
+    }
+}
---
+++ src/test/java/com/test/crss/AppTest.java
@@ -0,0 +1,16 @@
+package com.test.crss;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        CheckBoard checkBoard = new CheckBoard();
+        int[][] board = {{1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9}, {1, 2, 3, 4, 5, 6, 7, 8, 9}};
+        checkBoard.checkBoard(board);
+    }
+}

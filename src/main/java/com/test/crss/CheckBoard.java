c7c3b2cb931dfdc0c6116e3519d577322b12b5a2
Colette <colette.rs.shaw@gmail.com>
2020-05-18 18:33:14 +0100

    Write part of method that checks that each array is 1-9

--- src/main/java/com/test/crss/CheckBoard.java
+++ src/main/java/com/test/crss/CheckBoard.java
@@ -1,11 +1,26 @@
 package com.test.crss;
 
+import java.util.Arrays;
+
 public class CheckBoard {
     public static void main(String[] args) {
 
     }
 
-    public static boolean checkBoard(int[][] board) {
-        return true;
+    public static boolean checkBoard(String[][] board) {
+
+        // checks each array is 1-9
+        for (int i = 0; i < board.length; i++) {
+
+            String[] arrayCopy = board[i].clone();
+            String lineAsString = String.join("", arrayCopy);
+            char charArray[] = lineAsString.toCharArray();
+            Arrays.sort(charArray);
+            String sortedLineAsString = charArray.toString();
+
+            if (sortedLineAsString != "123456789") {
+                return false;
+            }
+        }
     }
 }

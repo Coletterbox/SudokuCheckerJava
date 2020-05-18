00dfcd90d06be64d57b877dcf14a08abe8c69bc4
Colette <colette.rs.shaw@gmail.com>
2020-05-18 19:49:45 +0100

    Add part of method that checks columns

--- src/main/java/com/test/crss/CheckBoard.java
+++ src/main/java/com/test/crss/CheckBoard.java
@@ -1,5 +1,6 @@
 package com.test.crss;
 
+import java.sql.Array;
 import java.util.Arrays;
 
 public class CheckBoard {
@@ -9,26 +10,39 @@ public class CheckBoard {
 
     public static boolean checkBoard(String[][] board) {
 
-        // checks each array is 1-9
+        // checks each array (i.e. each row) is 1-9
         for (int i = 0; i < board.length; i++) {
 
             String[] arrayCopy = board[i].clone();
             String lineAsString = String.join("", arrayCopy);
             char[] charArray = lineAsString.toCharArray();
             Arrays.sort(charArray);
-            System.out.println(charArray);
-            //String charArrayToString = Arrays.toString(charArray);
-            //System.out.println(charArrayToString);
-//            String sortedLineAsString = charArrayToString.join("");
-//
-//            System.out.println(sortedLineAsString);
-
-//            String sortedLineAsString = Arrays.toString(charArrayToString).substring(1, 3*charArrayToString.length()-1).replaceAll(", ", "");
-            String sortedLineAsString = "";
             String charArrayToString = new String(charArray);
-            System.out.println(charArrayToString);
+
             String sortedNumbers = "123456789";
             if (charArrayToString.compareTo(sortedNumbers) != 0) {
+                return false;
+            }
+        }
+
+        // rewrites board into arrays representing columns; checks columns
+        String[] columns = new String[81];
+        int columnsArrayIndex = 0;
+        for (int j = 0; j < board.length; j++) {
+            for (int k = 0; k < board[j].length; k++) {
+                columns[columnsArrayIndex] = board[k][j];
+                columnsArrayIndex++;
+            }
+        }
+        //System.out.println(Arrays.toString(columns));
+        for (int l = 0; l < 9; l++) {
+            int number = l*9;
+            String[] column = Arrays.copyOfRange(columns, number, number + 9);
+            String joinedColumn = String.join("", column);
+            System.out.println(joinedColumn);
+
+            String sortedNumbers = "123456789";
+            if (joinedColumn.compareTo(sortedNumbers) != 0) {
                 return false;
             }
         }

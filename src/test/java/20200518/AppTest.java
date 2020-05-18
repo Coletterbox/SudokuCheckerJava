61b32d5b75acc83982ef9238f41bcc5439023b50
Colette <colette.rs.shaw@gmail.com>
2020-05-18 16:19:36 +0100

    Initial commit

---
+++ src/test/java/20200518/AppTest.java
@@ -0,0 +1,20 @@
+package 20200518;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

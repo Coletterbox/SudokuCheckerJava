2bef3cda077db5875e272e8c6df6d8beac6d3e03
Colette <colette.rs.shaw@gmail.com>
2020-05-20 17:28:02 +0100

    Finish tests on existing solutions

--- src/test/java/com/test/crss/TestsToRunOnStoredSolutions.java
+++ src/test/java/com/test/crss/TestsToRunOnStoredSolutions.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
-public class TestSolutions {
+public class TestsToRunOnStoredSolutions {
     @Test
     public void checkForFalsePositives() {
         CheckBoard checkBoard = new CheckBoard();
@@ -116,5 +116,101 @@ public class TestSolutions {
                 {3,5,2, 1,7,4, 6,9,8}};
         String[][] boardAsStrings = CheckBoard.convertIntArrayToStringArray(boardAsInts);
         assertTrue(CheckBoard.checkBoard(boardAsStrings));
+    }
+
+    @Test
+    public void checkBoard6() {
+        CheckBoard checkBoard = new CheckBoard();
+        int[][] boardAsInts = {{1,3,2, 4,6,7, 5,8,9},
+                {4,6,5, 1,8,9, 2,3,7},
+                {7,8,9, 2,5,3, 1,4,6},
+                {8,2,4, 6,7,1, 3,9,5},
+                {3,5,7, 8,9,4, 6,2,1},
+                {9,1,6, 5,3,2, 4,7,8},
+                {6,7,3, 9,4,5, 8,1,2},
+                {5,4,1, 7,2,8, 9,6,3},
+                {2,9,8, 3,1,6, 7,5,4}};
+        String[][] boardAsStrings = CheckBoard.convertIntArrayToStringArray(boardAsInts);
+        assertTrue(CheckBoard.checkBoard(boardAsStrings));
+    }
+
+    @Test
+    public void checkBoard7() {
+        CheckBoard checkBoard = new CheckBoard();
+        int[][] boardAsInts = {{6,1,3, 4,5,7, 2,8,9},
+                {5,2,7, 1,8,9, 3,6,4},
+                {4,8,9, 3,2,6, 1,5,7},
+                {2,7,4, 5,6,3, 9,1,8},
+                {1,3,8, 9,4,2, 5,7,6},
+                {9,5,6, 7,1,8, 4,2,3},
+                {3,6,1, 2,7,4, 8,9,5},
+                {7,4,2, 8,9,5, 6,3,1},
+                {8,9,5, 6,3,1, 7,4,2}};
+        String[][] boardAsStrings = CheckBoard.convertIntArrayToStringArray(boardAsInts);
+        assertTrue(CheckBoard.checkBoard(boardAsStrings));
+    }
+
+    @Test
+    public void checkBoard8() {
+        CheckBoard checkBoard = new CheckBoard();
+        int[][] boardAsInts = {{2,3,4, 5,7,6, 9,8,1},
+                {1,5,6, 2,8,9, 4,3,7},
+                {7,8,9, 1,3,4, 2,6,5},
+                {3,1,2, 4,6,7, 8,5,9},
+                {9,6,8, 3,5,1, 7,2,4},
+                {4,7,5, 8,9,2, 6,1,3},
+                {6,4,1, 7,2,3, 5,9,8},
+                {8,2,7, 9,1,5, 3,4,6},
+                {5,9,3, 6,4,8, 1,7,2}};
+        String[][] boardAsStrings = CheckBoard.convertIntArrayToStringArray(boardAsInts);
+        assertTrue(CheckBoard.checkBoard(boardAsStrings));
+    }
+
+    @Test
+    public void checkBoard9() {
+        CheckBoard checkBoard = new CheckBoard();
+        int[][] boardAsInts = {{4,1,3, 6,5,7, 9,8,2},
+                {2,5,8, 1,3,9, 4,6,7},
+                {7,6,9, 8,2,4, 1,5,3},
+                {3,2,4, 7,6,8, 5,1,9},
+                {6,7,1, 2,9,5, 3,4,8},
+                {8,9,5, 4,1,3, 7,2,6},
+                {1,4,7, 3,8,2, 6,9,5},
+                {5,3,2, 9,4,6, 8,7,1},
+                {9,8,6, 5,7,1, 2,3,4}};
+        String[][] boardAsStrings = CheckBoard.convertIntArrayToStringArray(boardAsInts);
+        assertTrue(CheckBoard.checkBoard(boardAsStrings));
+    }
+
+    @Test
+    public void checkBoard10() {
+        CheckBoard checkBoard = new CheckBoard();
+        int[][] boardAsInts = {{9,7,8, 1,6,4, 5,3,2},
+                {2,5,1, 7,3,8, 9,4,6},
+                {4,3,6, 5,2,9, 1,8,7},
+                {7,4,5, 2,9,3, 6,1,8},
+                {6,9,3, 8,1,5, 7,2,4},
+                {1,8,2, 6,4,7, 3,9,5},
+                {8,6,4, 3,5,1, 2,7,9},
+                {3,2,9, 4,7,6, 8,5,1},
+                {5,1,7, 9,8,2, 4,6,3}};
+        String[][] boardAsStrings = CheckBoard.convertIntArrayToStringArray(boardAsInts);
+        assertTrue(CheckBoard.checkBoard(boardAsStrings));
+    }
+
+    @Test
+    public void okayLetsCheckForFalsePositivesAgain() {
+        CheckBoard checkBoard = new CheckBoard();
+        int[][] boardAsInts = {{9,7,8, 1,6,4, 5,3,2},
+                {2,5,1, 7,3,8, 9,4,6},
+                {4,3,6, 5,2,9, 1,8,7},
+                {7,4,5, 2,9,3, 6,1,8},
+                {6,9,3, 8,1,5, 7,2,4},
+                {1,8,2, 6,4,7, 3,9,5},
+                {8,6,4, 3,5,1, 2,7,9},
+                {3,2,9, 4,7,6, 8,5,1},
+                {5,1,7, 9,8,2, 4,6,2}};
+        String[][] boardAsStrings = CheckBoard.convertIntArrayToStringArray(boardAsInts);
+        assertFalse(CheckBoard.checkBoard(boardAsStrings));
     }
 }
